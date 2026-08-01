d930a04931d2deb89b66793db4378dce9218d197
Pushpinder <pushpinder.singh99991@gmail.com>
2026-08-01 11:39:16 +0530

    spring boot project 3

---
+++ springboot_git_3/src/main/java/com/example/springboot_git_3/SpringbootGit3Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot_git_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGit3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGit3Application.class, args);
+	}
+
+}
---
+++ springboot_git_3/src/test/java/com/example/springboot_git_3/SpringbootGit3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_git_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGit3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
