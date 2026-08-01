ad19736997ef3fda1ba4a5e1dde582f8e1a093c3
Pushpinder <pushpinder.singh99991@gmail.com>
2026-08-01 11:48:04 +0530

    saving file

--- springboot_git_3/src/main/java/com/example/springboot_git_3/SpringbootGit3Application.java
+++ springboot_git_3/src/main/java/com/example/springboot_git_3/SpringbootGit3Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootGit3Application {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SpringbootGit3Application.class, args);
 	}
 
