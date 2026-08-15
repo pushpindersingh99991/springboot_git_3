6bacebf68b21128530f0c36353ca6897437f6910
unknown <pushpinder.singh99991@gmail.com>
2026-08-15 16:26:42 +0530

    My Controller file by dev_9

--- springboot_git_3/src/main/java/com/example/springboot_git_3/MyController.java
+++ springboot_git_3/src/main/java/com/example/springboot_git_3/MyController.java
@@ -5,4 +5,9 @@ public class MyController {
 		public void display_9_1() {
 			System.out.println("9_1 Method");
 		}
+		
+		
+		public void display_9_2() {
+			System.out.println("9_2 Method");
+		}
 }
